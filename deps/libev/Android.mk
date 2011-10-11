d90d0b1165181ad280bc0d5929ad720d49168262
Paddy Byers <paddy.byers@gmail.com>
2011-10-11 18:54:02 +0100

    First attempt at Android.mk

---
+++ deps/libev/Android.mk
@@ -0,0 +1,34 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := ev 
+LOCAL_SRC_FILES := \
+	ev.c \
+#	ev_epoll.c \
+#	ev_kqueue.c \
+#	ev_poll.c \
+#	ev_port.c \
+	ev_select.c \
+	event.c
+
+LOCAL_CFLAGS += \
+	-DEV_FORK_ENABLE=0 \
+	-DEV_EMBED_ENABLE=0 \
+	-DEV_MULTIPLICITY=0 \
+	-include ev.h
+include $(BUILD_SHARED_LIBRARY)
