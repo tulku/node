7917d28d76b9403836955951a2577aeca132a014
Paddy Byers <paddy.byers@gmail.com>
2011-10-13 17:11:31 +0100

    Removed superfluous APP_MODULES

--- Application.mk
+++ Application.mk
@@ -1,4 +1,3 @@
 APP_PROJECT_PATH :=.
-APP_MODULES      := libnode deps/http_parser deps/libeio deps/libev deps/c-ares
 APP_BUILD_SCRIPT :=$(APP_PROJECT_PATH)/Android.mk
 APP_PLATFORM     := android-9
