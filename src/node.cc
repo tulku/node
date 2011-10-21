8f20b35856c8d26df217241f40fde1354ebc16be
Paddy Byers <paddy.byers@gmail.com>
2011-10-21 08:19:31 +0100

    Add condtitional for support of getgrnam_r and friends

--- src/node.cc
+++ src/node.cc
@@ -63,6 +63,9 @@ typedef int mode_t;
 #ifdef __POSIX__
 # include <pwd.h> /* getpwnam() */
 # include <grp.h> /* getgrnam() */
+# ifndef ANDROID
+#  define __REENTRANT_GRP__
+# endif
 #endif
 
 #include "platform.h"
@@ -1228,6 +1231,7 @@ Handle<Value> Isolate::SetGid(const Arguments& args) {
     gid = args[0]->Int32Value();
   } else if (args[0]->IsString()) {
     String::Utf8Value grpnam(args[0]->ToString());
+#if __REENTRANT_GRP__
     struct group grp, *grpp = NULL;
     int err;
 
@@ -1239,6 +1243,13 @@ Handle<Value> Isolate::SetGid(const Arguments& args) {
       else
         return ThrowException(isolate->ErrnoException(errno, "getgrnam_r"));
     }
+#else
+    struct group *grpp = NULL;
+
+    if ((grpp = getgrnam(*grpnam)) == NULL) {
+      return ThrowException(ErrnoException(errno, "getgrnam_r"));
+    }
+#endif //__REENTRANT_GRP__
 
     gid = grpp->gr_gid;
   } else {
@@ -1269,6 +1280,7 @@ Handle<Value> Isolate::SetUid(const Arguments& args) {
     uid = args[0]->Int32Value();
   } else if (args[0]->IsString()) {
     String::Utf8Value pwnam(args[0]->ToString());
+#if __REENTRANT_GRP__
     struct passwd pwd, *pwdp = NULL;
     int err;
 
@@ -1280,6 +1292,13 @@ Handle<Value> Isolate::SetUid(const Arguments& args) {
       else
         return ThrowException(isolate->ErrnoException(errno, "getpwnam_r"));
     }
+#else
+    struct passwd *pwdp = NULL;
+
+    if ((pwdp = getpwnam(*pwnam)) == NULL) {
+      return ThrowException(ErrnoException(errno, "getpwnam"));
+    }
+#endif //__REENTRANT_GRP__
 
     uid = pwdp->pw_uid;
   } else {
