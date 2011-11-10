51995cf4d043b6b21ada0cfa4fa1daf6ef81e460
Paddy Byers <paddy.byers@gmail.com>
2011-11-10 16:50:21 +0000

    Restore default behaviour

--- node-exe/node-exe/main.cpp
+++ node-exe/node-exe/main.cpp
@@ -4,26 +4,5 @@
 void *run(void *arg);
 
 int main(int argc, char **argv) {
-  pthread_t t1, t2;
-  int result = node::Initialize(argc, argv);
-  if(result == 0) {
-    pthread_create(&t1, NULL, &run, (void *)"1337");
-    pthread_create(&t2, NULL, &run, (void *)"1338");
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-    node::Dispose();
-  }
-  return result;
-}
-
-void *run(void *arg) {
-  char *arg0 = (char *)"node";
-  char *arg1 = (char *)"serv-hello.js";
-  char *arg2 = (char *)arg;
-  char *arg3 = 0;
-  char *argv[] = {arg0, arg1, arg2, arg3};
-  node::Isolate *isolate = node::Isolate::New();
-  isolate->Start(3, argv);
-  isolate->Dispose();
-  return NULL;
+  return node::Start(argc, argv);
 }
